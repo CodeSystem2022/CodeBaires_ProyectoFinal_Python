a0fbf093eb8d30a9962121f03909a121db7e9ae9
Matias Quispe <111471872+matiasq3@users.noreply.github.com>
2023-06-20 19:54:04 -0300

    displayCapuccino y displayComboBurguerEspecial
    agrego displayCapuccino y displayComboBurguerEspecial, y se configuran el estilo

--- test/pisplay.py
+++ test/pisplay.py
@@ -123,3 +123,79 @@ class Display:
             compound='bottom',
             padding=(2, 2, 2, 2)
         )
+
+def displayComboBurguerEspecial(comboBurguerBaconFrame, comboBurguerVeggieFrame, comboBurguerClasicaFrame, comboBurguerEspecialFrame,
+                                capuccinoFrame, espressoFrame, latteFrame, muffinArandanoFrame, muffinChipsFrame, muffinChocolateFrame,
+                                displayLabel, images):
+        # Instanciamos la imagen correspondiente al item del menú, la traemos de images.py
+        burguerEspecialImage = images['burguerEspecialImage']
+        comboBurguerEspecialFrame.configure(
+            relief='sunken',  # le asigna al marco o frame que va a contener este item un relieve hundido
+            style='selectedDish.TFrame' # le asigna el estilo configurado en la clase style para selectedDish que sería el item seleccionado
+        )
+        #  Configuramos el estilo del resto de los otros marcos
+        #  para asegurarnos de que solo el marco correspondiente esté distinto
+        comboBurguerBaconFrame.configure(style='DishFrame.TFrame')
+        comboBurguerVeggieFrame.configure(style='DishFrame.TFrame')
+        comboBurguerClasicaFrame.configure(style='DishFrame.TFrame')
+        capuccinoFrame.configure(style='DishFrame.TFrame')
+        espressoFrame.configure(style='DishFrame.TFrame')
+        latteFrame.configure(style='DishFrame.TFrame')
+        muffinArandanoFrame.configure(style='DishFrame.TFrame')
+        muffinChipsFrame.configure(style='DishFrame.TFrame')
+        muffinChocolateFrame.configure(style='DishFrame.TFrame')
+
+        # Configuramos el estilo para la imagen y el nombre que se va a ver en el sector display
+        # en el centro de nuestra aplicación
+        # image: trae  la imagen
+        # text: le asigna el texto
+        # font: le asigna el tipo de fuente, tamaño y estilo al texto
+        # foreground: es el color que va a tener el texto
+        # compound: es la alineación
+        # padding: es el espaciado entre los elementos
+        displayLabel.configure(
+            image=burguerEspecialImage,
+            text='Combo Burguer Especial',
+            font=('Franklin Gothic', 10, 'bold'),
+            foreground='white',
+            compound='bottom',
+            padding=(2, 2, 2, 2)
+        )
+
+    def displayCapuccino(comboBurguerBaconFrame, comboBurguerVeggieFrame, comboBurguerClasicaFrame, comboBurguerEspecialFrame,
+                                capuccinoFrame, espressoFrame, latteFrame, muffinArandanoFrame, muffinChipsFrame, muffinChocolateFrame,
+                                displayLabel, images):
+        # Instanciamos la imagen correspondiente al item del menú, la traemos de images.py
+        capuccinoImage = images['capuccinoImage']
+        capuccinoFrame.configure(
+            relief='sunken',  # le asigna al marco o frame que va a contener este item un relieve hundido
+            style='selectedDish.TFrame'# le asigna el estilo configurado en la clase style para selectedDish que sería el item seleccionado
+        )
+        #  Configuramos el estilo del resto de los otros marcos
+        #  para asegurarnos de que solo el marco correspondiente esté distinto
+        comboBurguerBaconFrame.configure(style='DishFrame.TFrame')
+        comboBurguerVeggieFrame.configure(style='DishFrame.TFrame')
+        comboBurguerClasicaFrame.configure(style='DishFrame.TFrame')
+        comboBurguerEspecialFrame.configure(style='DishFrame.TFrame')
+        espressoFrame.configure(style='DishFrame.TFrame')
+        latteFrame.configure(style='DishFrame.TFrame')
+        muffinArandanoFrame.configure(style='DishFrame.TFrame')
+        muffinChipsFrame.configure(style='DishFrame.TFrame')
+        muffinChocolateFrame.configure(style='DishFrame.TFrame')
+
+        # Configuramos el estilo para la imagen y el nombre que se va a ver en el sector display
+        # en el centro de nuestra aplicación
+        # image: trae  la imagen
+        # text: le asigna el texto
+        # font: le asigna el tipo de fuente, tamaño y estilo al texto
+        # foreground: es el color que va a tener el texto
+        # compound: es la alineación
+        # padding: es el espaciado entre los elementos
+        displayLabel.configure(
+            image=capuccinoImage,
+            text='Mc Capuccino',
+            font=('Franklin Gothic', 10, 'bold'),
+            foreground='white',
+            compound='bottom',
+            padding=(2, 2, 2, 2)
+        )
