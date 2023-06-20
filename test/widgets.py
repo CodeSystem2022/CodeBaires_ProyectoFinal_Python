95c1c06c50e40ddc997865c47eed327d80b798b9
Emmanueltoro28 <111504496+Emmanueltoro28@users.noreply.github.com>
2023-06-19 22:55:25 -0300

    subo widgets ultima parte

--- test/widgets.py
+++ test/widgets.py
@@ -98,3 +98,192 @@ class Widgets:
 
         muffinChipsLabel = ttk.Label(muffinChipsFrame, text='Muffin de Vainilla con chips - $550',
                                      style='MenuLabel.TLabel')
+        muffinChipsLabel.grid(row=0, column=0, padx=10, pady=10, sticky='W')
+
+        muffinChocolateLabel = ttk.Label(muffinChocolateFrame, text='Muffin de Chocolate - $550',
+                                         style='MenuLabel.TLabel')
+        muffinChocolateLabel.grid(row=0, column=0, padx=10, pady=10, sticky='W')
+
+        # Sección de pedido
+
+        # Estas etiquetas muestran los nombres en la seccion de pedido y definen su ubicación y estilo
+
+        # Etiqueta de Pedido
+        orderTitleLabel = ttk.Label(orderFrame, text='Pedido')
+        orderTitleLabel.grid(row=0, column=0, sticky='EW')
+        orderTitleLabel.configure(
+            foreground='white',
+            background='black',
+            font=('Franklin Gothic', 12, 'bold'),
+            anchor='center',
+            padding=(5, 5, 5, 5)
+        )
+
+        # Etiqueta de Id de Pedido
+        orderIDLabel = ttk.Label(orderFrame, text='Id. Pedido: ' + Order.order_id())
+        orderIDLabel.grid(row=1, column=0, sticky='EW', pady=1)
+        orderIDLabel.configure(
+            foreground='white',
+            background='black',
+            font=('Franklin Gothic', 12, 'normal'),
+            anchor='center',
+        )
+
+        # Etiqueta de la transacción (donde se ubican todos los items del menú seleccionados)
+        orderTransactionLabel = ttk.Label(orderFrame, style='orderTransaction.TLabel')
+        orderTransactionLabel.grid(row=2, column=0, sticky='NSEW')
+
+        # # Etiqueta de Total del Pedido
+        orderTotalLabel = ttk.Label(orderFrame, text='TOTAL: $0', style='orderTotal.TLabel')
+        orderTotalLabel.grid(row=3, column=0, sticky='EW')
+
+        # Sección del display (centro de la app)
+        displayLabel = ttk.Label(displayFrame, image=displayImage)
+        displayLabel.grid(row=0, column=0, sticky='NSEW', columnspan=2)
+        displayLabel.configure(background='#0F1110')
+
+        # Botones de la sección de Menú
+        # Se define tmb su ubicación, nombre y estilo
+        comboBurguerBaconButton = ttk.Button(comboBurguerBaconFrame, text='Mostrar',
+                                             command=lambda: Display.displayComboBurguerBacon(comboBurguerBaconFrame,
+                                                                                              comboBurguerVeggieFrame,
+                                                                                              comboBurguerClasicaFrame,
+                                                                                              comboBurguerEspecialFrame,
+                                                                                              capuccinoFrame,
+                                                                                              espressoFrame,
+                                                                                              latteFrame,
+                                                                                              muffinArandanoFrame,
+                                                                                              muffinChipsFrame,
+                                                                                              muffinChocolateFrame,
+                                                                                              displayLabel, images))
+        comboBurguerBaconButton.grid(row=0, column=1, padx=10)
+
+        comboBurguerClasicaButton = ttk.Button(comboBurguerClasicaFrame, text='Mostrar',
+                                               command=lambda: Display.displayComboBurguerClasica(
+                                                   comboBurguerBaconFrame,
+                                                   comboBurguerVeggieFrame,
+                                                   comboBurguerClasicaFrame,
+                                                   comboBurguerEspecialFrame,
+                                                   capuccinoFrame, espressoFrame,
+                                                   latteFrame,
+                                                   muffinArandanoFrame,
+                                                   muffinChipsFrame,
+                                                   muffinChocolateFrame,
+                                                   displayLabel, images))
+        comboBurguerClasicaButton.grid(row=0, column=1, padx=10)
+
+        comboBurguerEspecialButton = ttk.Button(comboBurguerEspecialFrame, text='Mostrar',
+                                                command=lambda: Display.displayComboBurguerEspecial(
+                                                    comboBurguerBaconFrame,
+                                                    comboBurguerVeggieFrame,
+                                                    comboBurguerClasicaFrame,
+                                                    comboBurguerEspecialFrame,
+                                                    capuccinoFrame,
+                                                    espressoFrame, latteFrame,
+                                                    muffinArandanoFrame,
+                                                    muffinChipsFrame,
+                                                    muffinChocolateFrame,
+                                                    displayLabel, images))
+        comboBurguerEspecialButton.grid(row=0, column=1, padx=10)
+
+        comboBurguerVeggieButton = ttk.Button(comboBurguerVeggieFrame, text='Mostrar',
+                                              command=lambda: Display.displayComboBurguerVeggie(comboBurguerBaconFrame,
+                                                                                                comboBurguerVeggieFrame,
+                                                                                                comboBurguerClasicaFrame,
+                                                                                                comboBurguerEspecialFrame,
+                                                                                                capuccinoFrame,
+                                                                                                espressoFrame,
+                                                                                                latteFrame,
+                                                                                                muffinArandanoFrame,
+                                                                                                muffinChipsFrame,
+                                                                                                muffinChocolateFrame,
+                                                                                                displayLabel, images))
+        comboBurguerVeggieButton.grid(row=0, column=1, padx=10)
+
+        capuccinoButton = ttk.Button(capuccinoFrame, text='Mostrar',
+                                     command=lambda: Display.displayCapuccino(comboBurguerBaconFrame,
+                                                                              comboBurguerVeggieFrame,
+                                                                              comboBurguerClasicaFrame,
+                                                                              comboBurguerEspecialFrame,
+                                                                              capuccinoFrame, espressoFrame, latteFrame,
+                                                                              muffinArandanoFrame, muffinChipsFrame,
+                                                                              muffinChocolateFrame,
+                                                                              displayLabel, images))
+        capuccinoButton.grid(row=0, column=1, padx=10)
+
+        espressoButton = ttk.Button(espressoFrame, text='Mostrar',
+                                    command=lambda: Display.displayEspresso(comboBurguerBaconFrame,
+                                                                            comboBurguerVeggieFrame,
+                                                                            comboBurguerClasicaFrame,
+                                                                            comboBurguerEspecialFrame,
+                                                                            capuccinoFrame, espressoFrame, latteFrame,
+                                                                            muffinArandanoFrame, muffinChipsFrame,
+                                                                            muffinChocolateFrame,
+                                                                            displayLabel, images))
+        espressoButton.grid(row=0, column=1, padx=10)
+
+        latteButton = ttk.Button(latteFrame, text='Mostrar',
+                                 command=lambda: Display.displayLatte(comboBurguerBaconFrame, comboBurguerVeggieFrame,
+                                                                      comboBurguerClasicaFrame,
+                                                                      comboBurguerEspecialFrame,
+                                                                      capuccinoFrame, espressoFrame, latteFrame,
+                                                                      muffinArandanoFrame, muffinChipsFrame,
+                                                                      muffinChocolateFrame,
+                                                                      displayLabel, images))
+        latteButton.grid(row=0, column=1, padx=10)
+
+        muffinArandanoButton = ttk.Button(muffinArandanoFrame, text='Mostrar',
+                                          command=lambda: Display.displayMuffinArandano(comboBurguerBaconFrame,
+                                                                                        comboBurguerVeggieFrame,
+                                                                                        comboBurguerClasicaFrame,
+                                                                                        comboBurguerEspecialFrame,
+                                                                                        capuccinoFrame, espressoFrame,
+                                                                                        latteFrame, muffinArandanoFrame,
+                                                                                        muffinChipsFrame,
+                                                                                        muffinChocolateFrame,
+                                                                                        displayLabel, images))
+        muffinArandanoButton.grid(row=0, column=1, padx=10)
+
+        muffinChipsButton = ttk.Button(muffinChipsFrame, text='Mostrar',
+                                       command=lambda: Display.displayMuffinChips(comboBurguerBaconFrame,
+                                                                                  comboBurguerVeggieFrame,
+                                                                                  comboBurguerClasicaFrame,
+                                                                                  comboBurguerEspecialFrame,
+                                                                                  capuccinoFrame, espressoFrame,
+                                                                                  latteFrame,
+                                                                                  muffinArandanoFrame, muffinChipsFrame,
+                                                                                  muffinChocolateFrame,
+                                                                                  displayLabel, images))
+        muffinChipsButton.grid(row=0, column=1, padx=10)
+
+        muffinChocolateButton = ttk.Button(muffinChocolateFrame, text='Mostrar',
+                                           command=lambda: Display.displayMuffinChocolate(comboBurguerBaconFrame,
+                                                                                          comboBurguerVeggieFrame,
+                                                                                          comboBurguerClasicaFrame,
+                                                                                          comboBurguerEspecialFrame,
+                                                                                          capuccinoFrame, espressoFrame,
+                                                                                          latteFrame,
+                                                                                          muffinArandanoFrame,
+                                                                                          muffinChipsFrame,
+                                                                                          muffinChocolateFrame,
+                                                                                          displayLabel, images))
+        muffinChocolateButton.grid(row=0, column=1, padx=10)
+
+        # Botones de la sección de Display (Sección central de la app)
+        # Se define tmb su ubicación, nombre y estilo
+        # Botón para agregar un item a la orden
+        addButton = ttk.Button(displayFrame, text='AGREGAR',
+                               command=lambda: Order.add(orderTransactionLabel, orderTotalLabel, displayLabel))
+        addButton.grid(row=1, column=0, padx=2, sticky='NSEW')
+        # Botón para eliminar un item de la orden
+        removeButton = ttk.Button(displayFrame, text='ELIMINAR',
+                                  command=lambda: Order.remove(orderTransactionLabel, orderTotalLabel, displayLabel))
+        removeButton.grid(row=1, column=1, padx=2, sticky='NSEW')
+
+        # Boton de la sección de Pedido
+        # Se define tmb su ubicación, nombre y estilo
+        # Este botón sirve para generar el ticket del pedido
+        orderButton = ttk.Button(orderFrame, text='PEDIR',
+                                 command=lambda: Order.order(orderTransactionLabel, orderIDLabel,
+                                                             orderTransactionLabel))
+        orderButton.grid(row=4, column=0, sticky='EW')
